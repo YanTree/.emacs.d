eed9a066e8ecf256fbea3b51cc348e4124ffc856
YanTree <yantreeland@outlook.com>
2023-07-21 11:17:02 +0800

    modify: update init.el/early-init.el file

--- early-init.el
+++ early-init.el
@@ -8,15 +8,10 @@
 ;;
 ;;; Code:
 
-;; PERF: Garbage collection is a big contributor to startup times. This fends it
-;;   off, but will be reset later by `gcmh-mode'. Not resetting it later will
-;;   cause stuttering/freezes.
-(setq gc-cons-threshold most-positive-fixnum)
-
 ;; PERF: Don't use precious startup time checking mtime on elisp bytecode.
 ;;   Ensuring correctness is 'doom sync's job, not the interactive session's.
 ;;   Still, stale byte-code will cause *heavy* losses in startup efficiency.
-(setq load-prefer-newer noninteractive)
+(setq load-prefer-newer t)
 
 ;; UX: Respect DEBUG envvar as an alternative to --debug-init, and to make are
 ;;   startup sufficiently verbose from this point on.
--- init.el
+++ init.el
@@ -4,28 +4,15 @@
 ;;
 ;;; Code:
 
-(require 'cl-lib)
 
-;;
-;;; Startup
-(progn
-  (defvar before-user-init-time (current-time)
-    "Value of `current-time' when Emacs begins loading `user-init-file'.")
-  (message "Loading Emacs...done (%.3fs)"
-           (float-time (time-subtract before-user-init-time
-                                      before-init-time)))
-  (setq user-init-file (or load-file-name buffer-file-name))
-  (setq user-emacs-directory (file-name-directory user-init-file))
-  (message "Loading %s..." user-init-file)
-  (when (< emacs-major-version 27)
-    (setq package-enable-at-startup nil)
-    ;; (package-initialize)
-    (load-file (expand-file-name "early-init.el" user-emacs-directory)))
-  (setq inhibit-startup-buffer-menu t)
-  (setq inhibit-startup-screen t)
-  (setq inhibit-startup-echo-area-message "locutus")
-  (setq initial-buffer-choice t))
-
+;; PERF: Garbage collection is a big contributor to startup times. This fends it
+;;   off, but will be reset later to normal. Not resetting it later will
+;;   cause stuttering/freezes.
+(let ((normal-gc-cons-threshold (* 16 1024 1024))
+      (init-gc-cons-threshold (* 512 1024 1024)))
+  (setq gc-cons-threshold init-gc-cons-threshold)
+  (add-hook 'emacs-startup-hook
+            (lambda () (setq gc-cons-threshold normal-gc-cons-threshold))))
 
 ;;
 ;;; About operating system
@@ -34,33 +21,8 @@
 (defconst IS-WINDOWS  (memq system-type '(cygwin windows-nt ms-dos)))
 (defconst IS-BSD      (memq system-type '(darwin berkeley-unix gnu/kfreebsd)))
 
-
-;;
-;;; Data directory
-
-(defvar cat-local-dir (expand-file-name "local/" user-emacs-directory)
-  )
-
-(defvar cat-cache-dir (expand-file-name "cache/" cat-local-dir)
-  )
-
-(defvar cat-data-dir (expand-file-name "data/" cat-local-dir)
-  )
-
-(defvar cat-modules-dir (expand-file-name "modules/" user-emacs-directory)
-  )
-
-(defvar cat-lisp-dir (expand-file-name "lisp/" user-emacs-directory)
-  )
-
-(defvar cat-templates-dir (expand-file-name "templates/" user-emacs-directory)
-  )
-
-(defconst cat-custom-example-file (expand-file-name "custom.example.el" cat-templates-dir)
-  "The path to example custom file")
-
-;;
-;;; Font
+;;; Font setting for default font, font for all unicode characters, emoji and
+;; chinese characters
 (defun font-installed-p (font-name)
   "Check if font with FONT-NAME is available."
   (find-font (font-spec :name font-name)))
@@ -109,6 +71,54 @@
 
 
 ;;
+;;; Startup
+(progn
+  (defvar before-user-init-time (current-time)
+    "Value of `current-time' when Emacs begins loading `user-init-file'.")
+  (message "Loading Emacs...done (%.3fs)"
+           (float-time (time-subtract before-user-init-time
+                                      before-init-time)))
+  (setq user-init-file (or load-file-name buffer-file-name))
+  (setq user-emacs-directory (file-name-directory user-init-file))
+  (message "Loading %s..." user-init-file)
+  (when (< emacs-major-version 27)
+    ;;; Suppress package.el
+    ;; Since Emacs 27, package initialization occurs before `user-init-file' is
+    ;; loaded, but after `early-init-file'. Doom handles package initialization, so
+    ;; we must prevent Emacs from doing it again.
+    (setq package-enable-at-startup nil)
+    ;; (package-initialize)
+    (load-file (expand-file-name "early-init.el" user-emacs-directory)))
+  (setq inhibit-startup-buffer-menu t)
+  (setq inhibit-startup-screen t)
+  (setq inhibit-startup-echo-area-message "locutus")
+  (setq initial-buffer-choice t))
+
+;;
+;;; Data directory
+(defvar cat-local-dir (expand-file-name "local/" user-emacs-directory)
+  "Root directory for local storage")
+
+(defvar cat-cache-dir (expand-file-name "cache/" cat-local-dir)
+  "Subfolder for `local', where stores its global cache files")
+
+(defvar cat-data-dir (expand-file-name "data/" cat-local-dir)
+  "Subfolder for `local', where stores its global data files")
+
+(defvar cat-modules-dir (expand-file-name "modules/" user-emacs-directory)
+  "Where stores all git submodules")
+
+(defvar cat-lisp-dir (expand-file-name "lisp/" user-emacs-directory)
+  "Where stores user personal config files")
+
+(defvar cat-templates-dir (expand-file-name "templates/" user-emacs-directory)
+  "Where sotres snippets and other template files")
+
+(defconst cat-custom-example-file (expand-file-name "custom.example.el" cat-templates-dir)
+  "The path to example custom file")
+
+
+;;
 ;;; Core 
 
 ;; Load `borg'
@@ -132,15 +142,178 @@
   (setq epkg-database-connector
         (if (>= emacs-major-version 29) 'sqlite-builtin 'sqlite-module)))
 
-(progn ;     startup
-  (message "Loading early birds...done (%.3fs)"
-           (float-time (time-subtract (current-time)
-                                      before-user-init-time))))
-
 
 ;; ---------------------------------------------------------------------------
 ;; Better default settings
 
+;;
+;;; Better default
+(progn
+  ;; Load custom.el file
+  (setq custom-file (expand-file-name "custom.el" cat-data-dir))
+  (when (file-exists-p custom-file)
+    (load custom-file))
+
+  ;; Load server
+  ;; (or (server-running-p) (server-mode))
+
+  ;; Paste overwrite marked text
+  (delete-selection-mode 1)
+
+  ;; Keep font caches to avoid compact during GC
+  (setq inhibit-compacting-font-caches t)
+
+  ;; Disable ring bell, it's annoying
+  (setq ring-bell-function #'ignore)
+
+  ;; Don't generate backups or lockfiles. While auto-save maintains a copy so long
+  ;; as a buffer is unsaved, backups create copies once, when the file is first
+  ;; written, and never again until it is killed and reopened. This is better
+  ;; suited to version control, and I don't want world-readable copies of
+  ;; potentially sensitive material floating around our filesystem.
+  (setq create-lockfiles nil
+        make-backup-files nil
+        ;; But in case the user does enable it, some sensible defaults:
+        version-control t     ; number each backup file
+        backup-by-copying t   ; instead of renaming current file (clobbers links)
+        delete-old-versions t ; clean up after itself
+        kept-old-versions 5
+        kept-new-versions 5
+        backup-directory-alist (list (cons "." (concat cat-data-dir "backup/")))
+        tramp-backup-directory-alist backup-directory-alist)
+
+  ;; But turn on auto-save, so we have a fallback in case of crashes or lost data.
+  ;; Use `recover-file' or `recover-session' to recover them.
+  (setq auto-save-default t
+        ;; Don't auto-disable auto-save after deleting big chunks. This defeats
+        ;; the purpose of a failsafe. This adds the risk of losing the data we
+        ;; just deleted, but I believe that's VCS's jurisdiction, not ours.
+        auto-save-include-big-deletions t
+        ;; Keep it out of `cat-emacs-dir' or the local directory.
+        auto-save-list-file-prefix (concat cat-data-dir "auto-save/sessions/")
+        tramp-auto-save-directory  (concat cat-data-dir "tramp/auto-save/")
+        tramp-persistency-file-name(concat cat-data-dir "tramp/persistency.el")
+        auto-save-file-name-transforms
+        (list (list "\\`/[^/]*:\\([^/]*/\\)*\\([^/]*\\)\\'"
+                    ;; Prefix tramp autosaves to prevent conflicts with local ones
+                    (concat auto-save-list-file-prefix "tramp-\\2") t)
+              (list ".*" auto-save-list-file-prefix t)))
+
+
+  ;;
+  ;;; Formatting
+
+  ;; Favor spaces over tabs, 8-space tabs more consistent
+  ;; 4-space tabs.
+  (setq-default indent-tabs-mode nil
+          tab-width 8)
+
+  ;; First hitting TAB tries to indent current line, if the line was already indent,
+  ;; then try to complete the thing at point
+  (setq tab-always-indent 'complete)
+
+  ;; Column beyond 80 which automatic line-wrapping should happen
+  (setq-default fill-column 80)
+
+
+  ;;
+  ;;; Line numbers
+
+  ;; Explicitly define a width to reduce the cost of on-the-fly computation
+  (setq-default display-line-numbers-width 3)
+
+  ;; Show absolute line numbers for narrowed regions to make it easier to tell the
+  ;; buffer is narrowed, and where you are, exactly.
+  (setq-default display-line-numbers-widen t)
+
+  ;; Enable line numbers in most text-editing modes. We avoid
+  ;; `global-display-line-numbers-mode' because there are many special and
+  ;; temporary modes where we don't need/want them.
+  (dolist (mode-alist '(prog-mode-hook
+                        text-mode-hook
+                        conf-mode-hook))
+    (add-hook mode-alist 'display-line-numbers-mode))
+
+
+  ;;
+  ;;; Scrolling
+
+  (setq hscroll-margin 2
+        hscroll-step 1
+        ;; Emacs spends too much effort recentering the screen if you scroll the
+        ;; cursor more than N lines past window edges (where N is the settings of
+        ;; `scroll-conservatively'). This is especially slow in larger files
+        ;; during large-scale scrolling commands. If kept over 100, the window is
+        ;; never automatically recentered.
+        scroll-conservatively 101
+        scroll-margin 0
+        scroll-preserve-screen-position t
+        ;; Reduce cursor lag by a tiny bit by not auto-adjusting `window-vscroll'
+        ;; for tall lines.
+        auto-window-vscroll nil)
+
+
+  ;;
+  ;;; Cursor
+
+  ;; The blinking cursor is distracting
+  (blink-cursor-mode -1)
+
+  ;; Don't stretch the cursor to fit wide characters, it is disorienting,
+  ;; especially for tabs.
+  (setq x-stretch-cursor nil)
+
+
+  ;;
+  ;;; Window/frames
+
+  ;; A simple frame title
+  (setq frame-title-format '("%b"))
+
+  ;; Disable GNU guide start screen
+  (setq inhibit-startup-screen t)
+
+  ;; Frame size
+  (add-to-list 'initial-frame-alist '(height . 40))
+  (add-to-list 'initial-frame-alist '(width . 120))
+
+
+  ;;
+  ;;; Minibuffer
+
+  ;; Allow for minibuffer-ception. Sometimes we need another minibuffer command
+  ;; while we're in the minibuffer.
+  (setq enable-recursive-minibuffers t)
+
+  ;; Show current key-sequence in minibuffer ala 'set showcmd' in vim. Any
+  ;; feedback after typing is better UX than no feedback at all.
+  (setq echo-keystrokes 0.02)
+
+  ;; Typeing yes/no is obnoxious when y/n will do
+  (if (boundp 'use-short-answers)
+      (setq use-short-answers t))
+
+  ;; Try to keep the cursor out of the read-only portions of the minibuffer.
+  (setq minibuffer-prompt-properties '(read-only t intangible t cursor-intangible t face minibuffer-prompt))
+  (add-hook 'minibuffer-setup-hook #'cursor-intangible-mode)
+
+
+  ;;
+  ;;; Extra file extensions to support
+
+  (nconc
+  auto-mode-alist
+  '(("/LICENSE\\'" . text-mode)
+    ("\\.log\\'" . text-mode)
+    ("rc\\'" . conf-mode)
+    ("\\.\\(?:hex\\|nes\\)\\'" . hexl-mode)))
+)
+
+;; Startup
+(progn
+  (message "Loading early birds...done (%.3fs)"
+           (float-time (time-subtract (current-time)
+                                      before-user-init-time))))
 
 ;;
 ;;; Built-in packages
@@ -311,170 +484,6 @@ the unwritable tidbits."
   
   ;; Avoid open too many dired buffer
   (put 'dired-find-alternate-file 'disabled nil))
-
-
-;;
-;;; Better default
-(progn
-  ;; Load custom.el file
-  (setq custom-file (expand-file-name "custom.el" cat-data-dir))
-  (when (file-exists-p custom-file)
-    (load custom-file))
-
-  ;; Load server
-  ;; (or (server-running-p) (server-mode))
-
-  ;; Paste overwrite marked text
-  (delete-selection-mode 1)
-
-  ;; Keep font caches to avoid compact during GC
-  (setq inhibit-compacting-font-caches t)
-
-  ;; Disable ring bell, it's annoying
-  (setq ring-bell-function #'ignore)
-
-  ;; Don't generate backups or lockfiles. While auto-save maintains a copy so long
-  ;; as a buffer is unsaved, backups create copies once, when the file is first
-  ;; written, and never again until it is killed and reopened. This is better
-  ;; suited to version control, and I don't want world-readable copies of
-  ;; potentially sensitive material floating around our filesystem.
-  (setq create-lockfiles nil
-        make-backup-files nil
-        ;; But in case the user does enable it, some sensible defaults:
-        version-control t     ; number each backup file
-        backup-by-copying t   ; instead of renaming current file (clobbers links)
-        delete-old-versions t ; clean up after itself
-        kept-old-versions 5
-        kept-new-versions 5
-        backup-directory-alist (list (cons "." (concat cat-data-dir "backup/")))
-        tramp-backup-directory-alist backup-directory-alist)
-
-  ;; But turn on auto-save, so we have a fallback in case of crashes or lost data.
-  ;; Use `recover-file' or `recover-session' to recover them.
-  (setq auto-save-default t
-        ;; Don't auto-disable auto-save after deleting big chunks. This defeats
-        ;; the purpose of a failsafe. This adds the risk of losing the data we
-        ;; just deleted, but I believe that's VCS's jurisdiction, not ours.
-        auto-save-include-big-deletions t
-        ;; Keep it out of `cat-emacs-dir' or the local directory.
-        auto-save-list-file-prefix (concat cat-data-dir "auto-save/sessions/")
-        tramp-auto-save-directory  (concat cat-data-dir "tramp/auto-save/")
-        tramp-persistency-file-name(concat cat-data-dir "tramp/persistency.el")
-        auto-save-file-name-transforms
-        (list (list "\\`/[^/]*:\\([^/]*/\\)*\\([^/]*\\)\\'"
-                    ;; Prefix tramp autosaves to prevent conflicts with local ones
-                    (concat auto-save-list-file-prefix "tramp-\\2") t)
-              (list ".*" auto-save-list-file-prefix t)))
-
-
-  ;;
-  ;;; Formatting
-
-  ;; Favor spaces over tabs, 8-space tabs more consistent
-  ;; 4-space tabs.
-  (setq-default indent-tabs-mode nil
-          tab-width 8)
-
-  ;; First hitting TAB tries to indent current line, if the line was already indent,
-  ;; then try to complete the thing at point
-  (setq tab-always-indent 'complete)
-
-  ;; Column beyond 80 which automatic line-wrapping should happen
-  (setq-default fill-column 80)
-
-
-  ;;
-  ;;; Line numbers
-
-  ;; Explicitly define a width to reduce the cost of on-the-fly computation
-  (setq-default display-line-numbers-width 3)
-
-  ;; Show absolute line numbers for narrowed regions to make it easier to tell the
-  ;; buffer is narrowed, and where you are, exactly.
-  (setq-default display-line-numbers-widen t)
-
-  ;; Enable line numbers in most text-editing modes. We avoid
-  ;; `global-display-line-numbers-mode' because there are many special and
-  ;; temporary modes where we don't need/want them.
-  (dolist (mode-alist '(prog-mode-hook
-                        text-mode-hook
-                        conf-mode-hook))
-    (add-hook mode-alist 'display-line-numbers-mode))
-
-
-  ;;
-  ;;; Scrolling
-
-  (setq hscroll-margin 2
-        hscroll-step 1
-        ;; Emacs spends too much effort recentering the screen if you scroll the
-        ;; cursor more than N lines past window edges (where N is the settings of
-        ;; `scroll-conservatively'). This is especially slow in larger files
-        ;; during large-scale scrolling commands. If kept over 100, the window is
-        ;; never automatically recentered.
-        scroll-conservatively 101
-        scroll-margin 0
-        scroll-preserve-screen-position t
-        ;; Reduce cursor lag by a tiny bit by not auto-adjusting `window-vscroll'
-        ;; for tall lines.
-        auto-window-vscroll nil)
-
-
-  ;;
-  ;;; Cursor
-
-  ;; The blinking cursor is distracting
-  (blink-cursor-mode -1)
-
-  ;; Don't stretch the cursor to fit wide characters, it is disorienting,
-  ;; especially for tabs.
-  (setq x-stretch-cursor nil)
-
-
-  ;;
-  ;;; Window/frames
-
-  ;; A simple frame title
-  (setq frame-title-format '("%b"))
-
-  ;; Disable GNU guide start screen
-  (setq inhibit-startup-screen t)
-
-  ;; Frame size
-  (add-to-list 'initial-frame-alist '(height . 40))
-  (add-to-list 'initial-frame-alist '(width . 120))
-
-
-  ;;
-  ;;; Minibuffer
-
-  ;; Allow for minibuffer-ception. Sometimes we need another minibuffer command
-  ;; while we're in the minibuffer.
-  (setq enable-recursive-minibuffers t)
-
-  ;; Show current key-sequence in minibuffer ala 'set showcmd' in vim. Any
-  ;; feedback after typing is better UX than no feedback at all.
-  (setq echo-keystrokes 0.02)
-
-  ;; Typeing yes/no is obnoxious when y/n will do
-  (if (boundp 'use-short-answers)
-      (setq use-short-answers t))
-
-  ;; Try to keep the cursor out of the read-only portions of the minibuffer.
-  (setq minibuffer-prompt-properties '(read-only t intangible t cursor-intangible t face minibuffer-prompt))
-  (add-hook 'minibuffer-setup-hook #'cursor-intangible-mode)
-
-
-  ;;
-  ;;; Extra file extensions to support
-
-  (nconc
-  auto-mode-alist
-  '(("/LICENSE\\'" . text-mode)
-    ("\\.log\\'" . text-mode)
-    ("rc\\'" . conf-mode)
-    ("\\.\\(?:hex\\|nes\\)\\'" . hexl-mode)))
-)
 
 
 ;; ---------------------------------------------------------------------------
