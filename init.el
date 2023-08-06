50a44d8a97bf196cdba62c989922ce89adc3bce7
YanTree <yantreeland@outlook.com>
2023-08-06 17:46:41 +0800

    modify: enable treesitter

--- init.el
+++ init.el
@@ -749,24 +749,25 @@ of project.el"
 ;; language server protocol
 
 ;; Treesitter
-;; (use-package treesit
-;;   :when (and (fboundp 'treesit-available-p)
-;;              (treesit-available-p))
-;;   :custom (major-mode-remap-alist
-;;            '((c-mode             . c-ts-mode)
-;;              (c++-mode           . c++-ts-mode)
-;;              (csharp-mode        . csharp-ts-mode)
-;;              (conf-toml-mode     . toml-ts-mode)
-;;              (javascript-mode    . js-ts-mode)
-;;              (js-json-mode       . json-ts-mode)
-;;              (sh-mode            . bash-ts-mode)
-;;              (python-mode        . python-ts-mode)))
-;;   :config
-;;   (add-to-list 'auto-mode-alist '("\\(?:CMakeLists\\.txt\\|\\.cmake\\)\\'" . cmake-ts-mode))
-;;   (add-to-list 'auto-mode-alist '("\\.rs\\'" . rust-ts-mode))
-;;   (add-to-list 'auto-mode-alist '("\\.ts\\'" . typescript-ts-mode))
-;;   (add-to-list 'auto-mode-alist '("\\.tsx\\'" . tsx-ts-mode))
-;;   (add-to-list 'auto-mode-alist '("\\.ya?ml\\'" . yaml-ts-mode)))
+(use-package treesit
+  :when (and (fboundp 'treesit-available-p)
+             (treesit-available-p))
+  :custom (major-mode-remap-alist
+           '((c-mode             . c-ts-mode)
+             (c++-mode           . c++-ts-mode)
+             (csharp-mode        . csharp-ts-mode)
+             (conf-toml-mode     . toml-ts-mode)
+             (javascript-mode    . js-ts-mode)
+             (js-json-mode       . json-ts-mode)
+             (sh-mode            . bash-ts-mode)
+             (python-mode        . python-ts-mode)))
+  :config
+  (add-to-list 'auto-mode-alist '("\\(?:CMakeLists\\.txt\\|\\.cmake\\)\\'" . cmake-ts-mode))
+  (add-to-list 'auto-mode-alist '("\\.rs\\'" . rust-ts-mode))
+  (add-to-list 'auto-mode-alist '("\\.wgsl\\'" . wgsl-mode))
+  (add-to-list 'auto-mode-alist '("\\.ts\\'" . typescript-ts-mode))
+  (add-to-list 'auto-mode-alist '("\\.tsx\\'" . tsx-ts-mode))
+  (add-to-list 'auto-mode-alist '("\\.ya?ml\\'" . yaml-ts-mode)))
 
 
 ;; ---------------------------------------------------------------------------
